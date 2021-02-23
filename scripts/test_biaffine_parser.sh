42eb6c986d32110c63fdf90ae5a3aaa78ac2d0ce
Marie Candito <mcandito@clc-llf-3.linguist.univ-paris-diderot.fr>
2021-02-23 12:54:43 +0100

    details

--- scripts/test_biaffine_parser.sh
+++ scripts/test_biaffine_parser.sh
@@ -13,10 +13,12 @@ D=$PROJH/../../git/marie-parsing/resources
 TRAIN_FILE=$D/FRENCH_SPMRL_dep_trees/train.French.predmorph.conll
 DEV_FILE=$D/FRENCH_SPMRL_dep_trees/dev.French.predmorph.conll
 
-# GRAPHS (TODO: predmorph version)
-TRAIN_FILE=$D/deep_french_dep_graphs/compacte.ftb.gold.dev
-#TRAIN_FILE=$D/deep_french_dep_graphs/compacte.ftb.gold.train
-DEV_FILE=$D/deep_french_dep_graphs/compacte.ftb.gold.dev
+# GRAPHS (predmorph version)
+#TRAIN_FILE=$D/deep_french_dep_graphs/compacte.ftb.gold.dev
+TRAIN_FILE=$D/deep_french_dep_graphs/compacte.ftb.predmorph.train
+DEV_FILE=$D/deep_french_dep_graphs/compacte.ftb.predmorph.dev
+
+DATA_NAME='ftb_predmorph'
 
 timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
 O=$PROJH/../OUTPUT/output-$timestamp
@@ -28,12 +30,12 @@ EMB_FILE=$D/vecs100-linear-frwiki
 W_EMB_SIZE=100
 L_EMB_SIZE=100
 P_EMB_SIZE=100
-R_BERT_SIZE=300
+R_BERT_SIZE=0 # no reduction
 LSTM_H_SIZE=600
-MLP_ARC_O_SIZE=200
-MLP_LAB_O_SIZE=200
+MLP_ARC_O_SIZE=600
+MLP_LAB_O_SIZE=600
 
-NB_EPOCHS=2 #40 #
+NB_EPOCHS=40 #
 NB_EPOCHS_ARC_ONLY=0
 LR=0.00002
 BATCH_SIZE=16
@@ -44,7 +46,8 @@ BERT_NAME=flaubert/flaubert_base_cased
 #BERT_NAME=''
 
 
-python $PROJH/biaffine_graph_parser/train_or_use_parser.py train $TRAIN_FILE $O -g -v $DEV_FILE -p $EMB_FILE -w $W_EMB_SIZE -l $L_EMB_SIZE -c $P_EMB_SIZE --reduced_bert_size $R_BERT_SIZE --lstm_h_size $LSTM_H_SIZE --mlp_arc_o_size $MLP_ARC_O_SIZE -b $BATCH_SIZE -r $LR -d $LEX_DROPOUT -i $LAB_LOSS_WEIGHT --pos_arc_weight $POS_ARC_WEIGHT -n $NB_EPOCHS --nb_epochs_arc_only $NB_EPOCHS_ARC_ONLY --device_id $DEVICE_ID --bert_name $BERT_NAME
+python $PROJH/train_or_use_parser.py train $TRAIN_FILE $O -g --data_name $DATA_NAME -v $DEV_FILE -p $EMB_FILE -w $W_EMB_SIZE -l $L_EMB_SIZE -c $P_EMB_SIZE --reduced_bert_size $R_BERT_SIZE --lstm_h_size $LSTM_H_SIZE --mlp_arc_o_size $MLP_ARC_O_SIZE --mlp_lab_o_size $MLP_LAB_O_SIZE -b $BATCH_SIZE -r $LR -d $LEX_DROPOUT -i $LAB_LOSS_WEIGHT --pos_arc_weight $POS_ARC_WEIGHT -n $NB_EPOCHS --nb_epochs_arc_only $NB_EPOCHS_ARC_ONLY --device_id $DEVICE_ID --bert_name $BERT_NAME
+
 
 # sans embeddings
-#python $PROJH/biaffine_graph_parser/train_or_use_parser.py train $TRAIN_FILE $O -g -v $DEV_FILE -w $W_EMB_SIZE -l $L_EMB_SIZE -c $P_EMB_SIZE --reduced_bert_size $R_BERT_SIZE --lstm_h_size $LSTM_H_SIZE --mlp_arc_o_size $MLP_ARC_O_SIZE -b $BATCH_SIZE -r $LR -d $LEX_DROPOUT -i $LAB_LOSS_WEIGHT --pos_arc_weight $POS_ARC_WEIGHT -n $NB_EPOCHS --nb_epochs_arc_only $NB_EPOCHS_ARC_ONLY --device_id $DEVICE_ID --bert_name $BERT_NAME
+#python $PROJH/train_or_use_parser.py train $TRAIN_FILE $O -g -v $DEV_FILE -w $W_EMB_SIZE -l $L_EMB_SIZE -c $P_EMB_SIZE --reduced_bert_size $R_BERT_SIZE --lstm_h_size $LSTM_H_SIZE --mlp_arc_o_size $MLP_ARC_O_SIZE --mlp_lab_o_size $MLP_LAB_O_SIZE -b $BATCH_SIZE -r $LR -d $LEX_DROPOUT -i $LAB_LOSS_WEIGHT --pos_arc_weight $POS_ARC_WEIGHT -n $NB_EPOCHS --nb_epochs_arc_only $NB_EPOCHS_ARC_ONLY --device_id $DEVICE_ID --bert_name $BERT_NAME
---
+++ scripts/tune_biaffine_parser.sh
@@ -0,0 +1,68 @@
+
+#set -x
+
+
+PROJH=$HOME/Documents/A_PROJETS/GRAPH_PARSER/git/marie-parsing
+# on CLC
+PROJH=/home/mcandito/A_PROJETS/GRAPH_PARSER/git/marie-parsing
+
+
+D=$PROJH/resources
+
+# TREES
+TRAIN_FILE=$D/FRENCH_SPMRL_dep_trees/train.French.predmorph.conll
+DEV_FILE=$D/FRENCH_SPMRL_dep_trees/dev.French.predmorph.conll
+
+# GRAPHS (predmorph version)
+#TRAIN_FILE=$D/deep_french_dep_graphs/compacte.ftb.gold.dev
+TRAIN_FILE=$D/deep_french_dep_graphs/compacte.ftb.predmorph.train
+DEV_FILE=$D/deep_french_dep_graphs/compacte.ftb.predmorph.dev
+DATA_NAME='ftb_predmorph'
+
+
+META_LOG=$PROJH/biaffine_graph_parser/OUTPUT/meta_log
+#META_LOG=$PROJH/biaffine_graph_parser/meta_log
+rm $META_LOG
+touch $META_LOG
+
+DEVICE_ID=0
+
+W_EMB_SIZE=100
+L_EMB_SIZE=100
+P_EMB_SIZE=100
+POS_ARC_WEIGHT=1.5
+
+NB_EPOCHS=40 #
+NB_EPOCHS_ARC_ONLY=0
+
+BATCH_SIZE=16
+
+BERT_NAME=flaubert/flaubert_base_cased
+#BERT_NAME=''
+
+#set -x
+
+
+for LSTM_H_SIZE in 800 600 400;
+do for LR in 0.00002 0.00005;
+   do for LAB_LOSS_WEIGHT in 0.5 0.3;
+      do for MLP_ARC_O_SIZE in 600 400; # used for ARC and LAB
+	 do  for R_BERT_SIZE in 0 300;
+	     do for LEX_DROPOUT in 0.3 0.4 0.5;
+		do for EMB_FILE in "" $D/vecs100-linear-frwiki ;
+		   do
+		       timestamp=$(date "+%Y-%m-%d-%H-%M-%S");
+		       O=$PROJH/biaffine_graph_parser/OUTPUT/output-$timestamp;
+		       mkdir $O;
+		       echo $O >> $META_LOG ;
+		       echo python $PROJH/biaffine_graph_parser/train_or_use_parser.py train $TRAIN_FILE $O -g --data_name $DATA_NAME -v $DEV_FILE -p $EMB_FILE -w $W_EMB_SIZE -l $L_EMB_SIZE -c $P_EMB_SIZE --reduced_bert_size $R_BERT_SIZE --lstm_h_size $LSTM_H_SIZE --mlp_arc_o_size $MLP_ARC_O_SIZE --mlp_lab_o_size $MLP_ARC_O_SIZE -b $BATCH_SIZE -r $LR -d $LEX_DROPOUT -i $LAB_LOSS_WEIGHT --pos_arc_weight $POS_ARC_WEIGHT -n $NB_EPOCHS --nb_epochs_arc_only $NB_EPOCHS_ARC_ONLY --device_id $DEVICE_ID --bert_name $BERT_NAME >> $META_LOG ;
+		       python $PROJH/biaffine_graph_parser/train_or_use_parser.py train $TRAIN_FILE $O -g --data_name $DATA_NAME -v $DEV_FILE -p $EMB_FILE -w $W_EMB_SIZE -l $L_EMB_SIZE -c $P_EMB_SIZE --reduced_bert_size $R_BERT_SIZE --lstm_h_size $LSTM_H_SIZE --mlp_arc_o_size $MLP_ARC_O_SIZE --mlp_lab_o_size $MLP_ARC_O_SIZE -b $BATCH_SIZE -r $LR -d $LEX_DROPOUT -i $LAB_LOSS_WEIGHT --pos_arc_weight $POS_ARC_WEIGHT -n $NB_EPOCHS --nb_epochs_arc_only $NB_EPOCHS_ARC_ONLY --device_id $DEVICE_ID --bert_name $BERT_NAME;
+		   done
+		done
+	     done
+	  done
+       done
+    done
+done
+
+       
