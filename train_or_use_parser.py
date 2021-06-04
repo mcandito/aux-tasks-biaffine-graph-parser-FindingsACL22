d3071969c8d357e6c8c901d4a5bfb163aabdab3f
candito <marie.candito@gmail.com>
2021-06-04 15:56:07 +0200

    aux_task_stack_propag

--- train_or_use_parser.py
+++ train_or_use_parser.py
@@ -61,6 +61,7 @@ if __name__ == "__main__":
     argparser.add_argument('--reduced_bert_size', help='If set to > 0, the bert embeddings will be linearly reduced before concat to non contextual embeddings. Default=0', type=int, default=0)    
     argparser.add_argument('--mlp_arc_o_size', help='size of arc mlp after lstm. Default=300', type=int, default=300)
     argparser.add_argument('--mlp_lab_o_size', help='size of lab mlp after lstm. Default=300', type=int, default=300)
+    argparser.add_argument('--aux_hidden_size', help='size of hidden layers of aux tasks. Default=300', type=int, default=300)
     argparser.add_argument('--bert_name', help='huggingface *bert model name. If not "None", will be used as pretrained-LM. Default:flaubert/flaubert_base_cased', default="flaubert/flaubert_base_cased")
     argparser.add_argument('-f', '--freeze_bert', action="store_true", help='Whether to freeze *bert parameters. Default=False', default=False)
     argparser.add_argument('--use_bias', action="store_true", help='Whether to add bias in all internal MLPs. Default=True', default=True)
@@ -74,6 +75,7 @@ if __name__ == "__main__":
     argparser.add_argument('-n', '--nb_epochs', help='Max nb epochs. Default=40', type=int, default=40)
     argparser.add_argument('--mtl_sharing_level', help='Sharing level for MTL. 1: shared until bilstm, 2:extra MLP shared. Default: 1', type=int, default=1)
     argparser.add_argument('--coeff_aux_task_as_input', help='"None" or dot-separated list of task:coeff pairs, with task s and/or h. Example: "s:5.h15", Default=""', default='None')
+    argparser.add_argument('--coeff_aux_task_stack_propag', help='"None" or dot-separated list of task:coeff pairs, with task s and/or h. Example: "b:1.h:10.s:2", Default=""', default='None')
 #    argparser.add_argument('--nb_epochs_arc_only', help='Nb epocs to run using arc loss only. Default=0', type=int, default=0)
     argparser.add_argument('--device_id', help='GPU cuda device id. Default=0', type=int, default=0)
     argparser.add_argument('-t', '--trace', action="store_true", help='print some traces. Default=False', default=False)
@@ -145,17 +147,28 @@ if __name__ == "__main__":
         tasks = args.tasks.lower().split('.')
         if sum([ int(t not in ['a','l','h','d','s','b']) for t in tasks ]) > 0:
           exit("ERROR: tasks should be among a l h d s b")
-        coeffs = {}
+        coeff_aux_task_as_input = {}
         if args.coeff_aux_task_as_input != 'None':
           c = args.coeff_aux_task_as_input
           for x in c.strip().split('.'):
             (t, v) = x.split(':')
             v = int(v)
-            if t in tasks and t in ['s', 'h']:
-                coeffs[t] = v
+            if t in tasks and t in ['s', 'h', 'b']:
+                coeff_aux_task_as_input[t] = v
             else:
                 print("WARNING coeff_aux_task_as_input %s incoherent, skipping %s : %d " % (c, t, v))
-            
+
+        coeff_aux_task_stack_propag = {}
+        if args.coeff_aux_task_stack_propag != 'None':
+          c = args.coeff_aux_task_stack_propag
+          for x in c.strip().split('.'):
+            (t, v) = x.split(':')
+            v = int(v)
+            if t in tasks:
+                coeff_aux_task_stack_propag[t] = v
+            else:
+                print("WARNING coeff_aux_task_stack_propag %s incoherent with tasks, skipping %s : %d " % (c, t, v))
+          
 
         # ------------- INDICES ------------------------------
         # indices are defined on train sentences only
@@ -179,14 +192,16 @@ if __name__ == "__main__":
                                         lstm_h_size=args.lstm_h_size,
                                         lstm_dropout=args.lstm_dropout,
                                         mlp_arc_o_size=args.mlp_arc_o_size, 
-                                        mlp_lab_o_size=args.mlp_lab_o_size, 
+                                        mlp_lab_o_size=args.mlp_lab_o_size,
+                                        aux_hidden_size=args.aux_hidden_size,
                                         use_pretrained_w_emb=use_pretrained_w_emb, 
                                         use_bias=args.use_bias,
                                         bert_name=args.bert_name,
                                         reduced_bert_size=args.reduced_bert_size,
                                         freeze_bert=args.freeze_bert,
                                         mtl_sharing_level=args.mtl_sharing_level,
-                                        coeff_aux_task_as_input=coeffs,
+                                        coeff_aux_task_as_input=coeff_aux_task_as_input,
+                                        coeff_aux_task_stack_propag=coeff_aux_task_stack_propag,
         )
 
         # pour tests plus rapides: utiliser training sur val
